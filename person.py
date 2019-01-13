7959263a0450dff4acab2736703acab4cfdfe542
ice cold <icemasterpt@gmail.com>
2019-01-13 15:24:54 +0000

    Novos ficheiros

---
+++ person.py
@@ -0,0 +1,20 @@
+class gender:
+    name = "null"
+    def __init__(self,name):
+        self.name = name
+
+class person:
+    name = "null"
+    age = -1
+    country = "null"
+    money = -1
+    gender = 0
+
+
+    def __init__(self,name,gender,age):
+        self.name = name
+        self.age = age
+        self.gender = gender
+    
+
+    
